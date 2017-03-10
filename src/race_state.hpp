7fbf39a9438c601980c1056246c1058b0488a84f
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2017-03-10 15:44:41 -0300

    First cleanup for new pseudo3d code. Test draw of a quad.

--- src/race_state.hpp
+++ src/race_state.hpp
@@ -10,16 +10,8 @@
 #include <ciso646>
 
 #include "carse_game.hpp"
-
-#include "racing/vehicle.hpp"
-
 #include "fgeal/fgeal.hpp"
-
 #include "futil/general/language.hpp"
-
-#include <Box2D/Box2D.h>
-
-#include "util/box2d_util.hpp"
 
 class RaceState extends public fgeal::Game::State
 {
@@ -27,23 +19,7 @@ class RaceState extends public fgeal::Game::State
 	int getId() { return CarseGame::RACE_STATE_ID; }
 
 	private:
-	b2World* world;
-	Car* player;
-
-	bool lockOn;  // glitches on SDL adapters
-	bool showDebug; // crashes on allegro adapter
-
-	//the race camera
-	Rect camera;
-	double cameraAngle;
-
-	fgeal::Image* car_sprite, *track_bg;
-	fgeal::Sound* car_sound_idle, *car_sound_high;
-	fgeal::Music* music_sample;
 	fgeal::Font* font, *font2;
-
-	bool isKeyUpPressed, isKeyDownPressed,
-		 isKeyRightPressed, isKeyLeftPressed;
 
 	char buffer[256];
 

3ae2793aa28524f6ec55698d18ce857fe092456d
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2018-02-21 15:43:46 -0300

    Added initial header fields for custom vehicle shadow.

--- src/pseudo3d/vehicle_gfx.hpp
+++ src/pseudo3d/vehicle_gfx.hpp
@@ -17,6 +17,8 @@
 /** A class containing data used to draw pseudo-3D vehicle animations. */
 struct Pseudo3DVehicleAnimationSpec
 {
+	// Main vehicle sprites
+
 	/** The filename of the image containing the sprite sheet. */
 	std::string sheetFilename;
 
@@ -54,6 +56,8 @@ struct Pseudo3DVehicleAnimationSpec
 	 *  effects, such as burning rubber's smoking animation, etc. */
 	unsigned depictedVehicleWidth;
 
+	// Brakelights sprites
+
 	/** The filename of the image containing the sprite for the optional brakelights overlay.
 	 *  Note that the actual brakelight image portraited in the sheet should be in the same scale as
 	 *  the vehicle sprite sheet.*/
@@ -63,8 +67,8 @@ struct Pseudo3DVehicleAnimationSpec
 	fgeal::Vector2D brakelightsSpriteScale;
 
 	/** The positions of the brakelights within the animation's coordinates.
-	 *  Note that this is vector because it's needed to specify the brakelight position on each of
-	 *  this animation's states. */
+	 *  Note that this is a vector because each of the vehicle animation's states may have the
+	 *  brakelights located at different coordinates. */
 	std::vector<fgeal::Point> brakelightsPositions;
 
 	/** An optional offset applied to the brakelights sprites' positions, meant to center them when
@@ -81,6 +85,23 @@ struct Pseudo3DVehicleAnimationSpec
 	 *  multiple sprites, one per each animation state; the brakelight sheet is also assumed to contain
 	 *  N equally sized frames, where N is the number of states of this animation. */
 	bool brakelightsMultipleSprites;
+
+	// Shadow layer/sprites
+
+	/** The filename of the image containing the sprite for the optional shadow overlay.
+	 *  If this field is specified, the shadow sprite will be drawn before the vehicle sprite.
+	 *  Note that this sprites should depict the vehicle's shadow as it was being cast from
+	 *  top-down position. Also, the shadow image should be in the same scale as the vehicle sprite.
+	 *  If this field is assigned 'none' or is left unspecified, it's assumed that there is no shadow
+	 *  to draw/cast. If 'default' or 'builtin' is specified, a black, translucent ellipsis is drawn
+	 *  below the vehicle, with a width equivalent to 95% of the depicted vehicle width. */
+	std::string shadowSheetFilename;
+
+	/** The positions of the shadows within the animation's coordinates.
+	 *  Note that this is a vector because each of the vehicle animation's states may have the
+	 *  shadow located at different coordinates.
+	 *  This value is ignored if a default shadow is used.*/
+	std::vector<fgeal::Point> shadowPositions;
 };
 
 #endif /* PSEUDO3D_VEHICLE_GFX_HPP_ */

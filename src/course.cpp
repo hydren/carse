f30941ee7fde5d6009f60f5f97752964805bb2ea
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2018-09-27 12:46:03 -0300

    Added sanity check in Pseudo3DCourse::draw() method.

--- src/course.cpp
+++ src/course.cpp
@@ -67,6 +67,9 @@ namespace // static
 
 void Pseudo3DCourse::draw(int pos, int posX)
 {
+	if(spec.lines.empty() or spec.roadSegmentLength == 0)
+		return;
+
 	const unsigned N = spec.lines.size(), fromPos = pos/spec.roadSegmentLength;
 	const float camHeight = 1500 + spec.lines[fromPos].y;
 	float x = 0, dx = 0;

14999affba22ade858b16156955b05fe217e6ffd
Carlos Faruolo <5carlosfelipe5@gmail.com>
2018-12-28 01:48:46 -0200

    Added makefiles to work with Allegro 4.4.

---
+++ build/backend-makefiles/linux-gcc-allegro4.4-release/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lalleg -lloadpng -ljpgalleg -lalgif -llogg -lalmp3 -lalttf
+

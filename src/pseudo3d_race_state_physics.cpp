1148c3ec09c5dc08d7c858214ed441036bd14b71
Carlos F. M. Faruolo <carlosfelipe.faruolo@unirio.br>
2019-04-17 17:42:54 -0300

    Slightly more efficient check for traffic collision.

--- src/pseudo3d_race_state_physics.cpp
+++ src/pseudo3d_race_state_physics.cpp
@@ -47,7 +47,8 @@ static const float TIRE_FRICTION_COEFFICIENT_DRY_ASPHALT = 0.85,
 
 void Pseudo3DRaceState::handlePhysics(float delta)
 {
-	const CourseSpec::Segment& courseSegment = course.spec.lines[((int)(playerVehicle.position*coursePositionFactor/course.spec.roadSegmentLength))%course.spec.lines.size()];
+	const unsigned courseSegmentIndex = static_cast<int>(playerVehicle.position * coursePositionFactor / course.spec.roadSegmentLength) % course.spec.lines.size();
+	const CourseSpec::Segment& courseSegment = course.spec.lines[courseSegmentIndex];
 	const float corneringForceLeechFactor = (playerVehicle.body.vehicleType == Mechanics::TYPE_BIKE? 0.25 : 0.5),
 				wheelAngleFactor = 1 - corneringForceLeechFactor*fabs(playerVehicle.pseudoAngle)/PSEUDO_ANGLE_MAX,
 				maxStrafeSpeed = MAXIMUM_STRAFE_SPEED_FACTOR * coursePositionFactor * playerVehicle.corneringStiffness;
@@ -142,9 +143,8 @@ void Pseudo3DRaceState::handlePhysics(float delta)
 		trafficVehicle.body.updatePowertrain(delta);
 		trafficVehicle.position += coursePositionFactor*trafficVehicle.body.speed*delta;  // update position
 
-		const CourseSpec::Segment& trafficVehicleSegment = course.spec.lines[((int)((trafficVehicle.position - PLAYER_VEHICLE_PROJECTION_OFFSET*coursePositionFactor)/course.spec.roadSegmentLength))%course.spec.lines.size()];
-
-		if(&trafficVehicleSegment == &courseSegment)  // if on the same segment, check for collision
+		const unsigned trafficVehicleCourseSegmentIndex = static_cast<int>((trafficVehicle.position - PLAYER_VEHICLE_PROJECTION_OFFSET * coursePositionFactor) / course.spec.roadSegmentLength) % course.spec.lines.size();
+		if(trafficVehicleCourseSegmentIndex == courseSegmentIndex)  // if on the same segment, check for collision
 		{
 			const float pw = playerVehicle.spriteSpec.depictedVehicleWidth * playerVehicle.sprites.back()->scale.x * 7,
 						px = playerVehicle.horizontalPosition - 0.5f*pw,

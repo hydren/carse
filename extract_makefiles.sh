63283f260cebd79eabe8e36d701e8fc7bc79fe06
hydren <5carlosfelipe5@gmail.com>
2019-06-05 14:09:15 -0300

    Using fgeal 0.8-dev. Renamed build configurations. Removed old entries.

--- extract_makefiles.sh
+++ extract_makefiles.sh
@@ -2,15 +2,15 @@
 echo "### Preparing linux makefiles for prime time ###"
 
 # These need to be in par with the current build configuration names. Be sure that these are updated.
-BUILD_CONFIG_ALLEGRO_4_4=linux-gcc-allegro4.4-release
-BUILD_CONFIG_ALLEGRO_5_0=linux-gcc-allegro5.0-release
-BUILD_CONFIG_SDL_1_2=linux-gcc-sdl1.2-release
-BUILD_CONFIG_SDL_2_0=linux-gcc-sdl2.0-release
-BUILD_CONFIG_SFML_1_6=linux-gcc-sfml1.6-release
-BUILD_CONFIG_SFML_2_0=linux-gcc-sfml2.0-release
+BUILD_CONFIG_ALLEGRO4=linux-gcc-allegro4-release
+BUILD_CONFIG_ALLEGRO5=linux-gcc-allegro5-release
+BUILD_CONFIG_SDL=linux-gcc-sdl-release
+BUILD_CONFIG_SDL2=linux-gcc-sdl2-release
+BUILD_CONFIG_SFML=linux-gcc-sfml-release
+BUILD_CONFIG_SFML2=linux-gcc-sfml2-release
 
 # One of the configs. will be used to construct the backend-agnostic makefiles
-BUILD_CONFIG_AGNOSTIC=$BUILD_CONFIG_SDL_1_2
+BUILD_CONFIG_AGNOSTIC=$BUILD_CONFIG_SDL
 
 function patch_makefiles {
 	local DPATH=$1
@@ -47,7 +47,7 @@ mv build/objects.mk build/backend-makefiles/$BUILD_CONFIG_AGNOSTIC/
 mv build/sources.mk build/backend-makefiles/$BUILD_CONFIG_AGNOSTIC/
 
 echo "- Dealing with remaining makefiles that are specific to other build configurations..."
-for BUILD_CONFIG in $BUILD_CONFIG_ALLEGRO_4_4 $BUILD_CONFIG_ALLEGRO_5_0 $BUILD_CONFIG_SDL_2_0 $BUILD_CONFIG_SFML_1_6 $BUILD_CONFIG_SFML_2_0
+for BUILD_CONFIG in $BUILD_CONFIG_ALLEGRO4 $BUILD_CONFIG_ALLEGRO5 $BUILD_CONFIG_SDL2 $BUILD_CONFIG_SFML $BUILD_CONFIG_SFML2
 do
 	echo "- Erasing contents of build/backend-makefiles/$BUILD_CONFIG folder (except configure.sh)..."
 	mv build/backend-makefiles/$BUILD_CONFIG/configure.sh .
